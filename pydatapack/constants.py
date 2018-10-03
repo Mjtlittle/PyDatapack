1d736751c8f2cb53592c5e7d778d99bb54a13d7f
Joshua <Jplittle2001@gmail.com>
2018-10-02 20:33:22 -0400

    add colors as a constant

--- pydatapack/constants.py
+++ pydatapack/constants.py
@@ -5,6 +5,7 @@ TAG_ONLOAD = 'minecraft:load'
 TAG_ONTICK = 'minecraft:tick'
 PLAYER_HEAD_HEIGHT = 1.62000000476837
 AIR_BLOCKS = ['minecraft:air','minecraft:cave_air','minecraft:void_air']
+COLORS = ['white','orange','magenta','light_blue','yellow','lime','pink','gray','light_gray','cyan','purple','blue','brown','green','red','black']
 INTERNAL_PREFIX = 'internal/'
 VALID_OBJECTIVE_CHARACTERS = list(string.ascii_letters + string.digits + '_-.+')
 
